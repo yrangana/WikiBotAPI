359c7f54afa393b183e7a6a1f296a90245420351
Yasiru Rangana <yrangana@gmail.com>
2024-02-08 14:09:29 +1100

    minor formatting changes

--- tests/test_scrapebot.py
+++ tests/test_scrapebot.py
@@ -1,8 +1,7 @@
 from scrapebot.bot import get_wikipedia_summary
 
+
 def test_get_wikipedia_summary():
-    assert "Ceylon"  in get_wikipedia_summary("Sri Lanka", 1)
+    assert "Ceylon" in get_wikipedia_summary("Sri Lanka", 1)
     assert "Sri Lanka" in get_wikipedia_summary("Sri Lanka", 2)
     assert "No results found" in get_wikipedia_summary("Sri Lanka123", 2)
-    
-    
