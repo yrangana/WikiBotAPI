32037467835d7aee7d735503af8a8d8391bff605
Yasiru Rangana <yrangana@gmail.com>
2024-02-08 14:10:09 +1100

    fast api implemented

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+
 from scrapebot.bot import get_wikipedia_summary
 
+app = FastAPI()
+
+
+class WikipediaSummaryRequest(BaseModel):
+    title: str
+    sentences: int
+
+
+@app.post("/wikipedia_summary")
+async def wikipedia_summary(request: WikipediaSummaryRequest):
+    summary = get_wikipedia_summary(request.title, request.sentences)
+    return JSONResponse(content=jsonable_encoder(summary))
+
+
+@app.get("/")
+async def root():
+    return {"message": "WikiBot API"}
+
+
 if __name__ == "__main__":
-    print(get_wikipedia_summary("Sri Lanka", 2))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
