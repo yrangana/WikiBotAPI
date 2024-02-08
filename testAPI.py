a86255192333adec4f47a8b9cfc779d5c0605c37
Yasiru Rangana <yrangana@gmail.com>
2024-02-08 17:11:55 +1100

    API test file created

---
+++ testAPI.py
@@ -0,0 +1,14 @@
+#test hosted API in amazon app runner
+import requests
+import json
+
+url = "http://0.0.0.0:8081/wikipedia_summary"
+
+data = {
+    "title": "Microsoft",
+    "sentences": 2
+}
+
+response = requests.post(url, json=data)
+
+print(json.dumps(response.json(), indent=2))
